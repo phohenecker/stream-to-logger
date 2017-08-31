812216d0f3e2e61d7ced17bc2109d25b50ccff8d
Patrick Hohenecker <mail@paho.at>
2017-08-31 16:53:20 +0200

    set up testing

---
+++ run_tests.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# MIT License
+#
+# Copyright (c) 2017 Patrick Hohenecker
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# author:   Patrick Hohenecker <mail@paho.at>
+# version:  2017.1
+# date:     Aug 31, 2017
+
+
+export PYTHONPATH=`pwd`/src/main/python:${PYTHONPATH}
+python3 -m unittest discover -s src/test/python -p "*_test.py"
